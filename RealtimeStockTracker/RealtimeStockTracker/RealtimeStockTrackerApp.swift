d5b58c73e948ddc6d9b250d052c8830b51164077
akkisabharwal90 <akkisabharwal90@gmail.com>
2026-05-08 17:54:30 +0400

    Initial project setup

---
+++ RealtimeStockTracker/RealtimeStockTracker/RealtimeStockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeStockTrackerApp.swift
+//  RealtimeStockTracker
+//
+//  Created by Ankit Sabharwal on 08/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeStockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
